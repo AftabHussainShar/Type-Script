293f687a49fe6d130a2a3d92a337d5efe06e2525
Aftab Hussain <96955752+AftabHussainShar@users.noreply.github.com>
2024-03-15 14:47:08 +0500

    Variables Advance

---
+++ 04-Variables-Advance/main.ts
@@ -0,0 +1,55 @@
+// variables advance 
+
+// 1:let variable
+// 2:const variable
+// 3:var variable
+
+// 1:let variable 
+// points:
+// 1:it can be changed
+// 2:it can be used in loop
+// 3:it can be used in function
+// 4:it is new way to declare variable  
+
+let username = "Aftab"
+let age = 25
+let isMarried = true
+let favColor = "blue"
+
+console.log(username)
+console.log(age)
+console.log(isMarried)
+console.log(favColor)
+
+
+// 2:const variable
+// points:
+// 1:it can't be changed
+// 2:it can be used in loop
+// 3:it can be used in function
+
+const username2 = "Aftab"
+const age2 = 25
+const isMarried2 = true
+const favColor2 = "blue"
+// age2 = 26  //Here it genrate error because it can't be changed
+console.log(username2)
+console.log(age2)
+console.log(isMarried2)
+console.log(favColor2)
+
+
+//3:var variable 
+// points:
+// 1:it can be changed
+// 2:it can be used in loop
+// 3:it can be used in function
+// 4:it is old way to declare variable  this is golbel
+var username3 = "Aftab"
+var age3 = 25
+var isMarried3 = true
+var favColor3 = "blue"
+console.log(username3)
+console.log(age3)
+console.log(isMarried3)
+console.log(favColor3)
