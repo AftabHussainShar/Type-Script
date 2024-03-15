262eb50e4ff65622ae177dba4e74a1dfd7c3c874
Aftab Hussain <96955752+AftabHussainShar@users.noreply.github.com>
2024-03-15 14:53:06 +0500

    Errors

---
+++ 05-Errors/main.js
@@ -0,0 +1,13 @@
+"use strict";
+// Errors
+// 1. Syntax Error
+// let username = "Aftab"
+// console.log(username    //here it will give error beacause of syntax error i did not use semicolon
+// 2. Type Error
+// let username :string = "Aftab"
+// username = 1  // here it will give error beacause of type error i did not use string
+// console.log(username)
+// 3. Asignability Error
+// let username = "Aftab"
+// username = 1  // here it will give error beacause of type error i did not use string
+// console.log(username)
