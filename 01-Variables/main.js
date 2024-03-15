bb411a7e8de5f1f99aa402682e0909ea3ad23bba
Aftab Hussain <96955752+AftabHussainShar@users.noreply.github.com>
2024-03-15 14:20:56 +0500

    Code

--- 01-Variables/main.js
+++ 01-Variables/main.js
@@ -1,3 +1,18 @@
 "use strict";
+// making new variable 
 let fav_Color = "blue";
 console.log("My favorite color is " + fav_Color);
+//change value of variable
+fav_Color = "green";
+console.log("My favorite color is " + fav_Color);
+//case sensitive
+// 1: camelCase
+// 2: PascalCase
+// 3: snake_case
+// 4: kebab-case
+// only before $ and _ can be used
+// strong typing
+let userName = "Aftab";
+console.log(userName);
+// here i can only use string
+// userName = 1    // its giving error
--- 01-Variables/main.ts
+++ 01-Variables/main.ts
@@ -1,2 +1,23 @@
+// making new variable 
 let fav_Color = "blue"
 console.log("My favorite color is " + fav_Color)
+
+//change value of variable
+fav_Color = "green"
+console.log("My favorite color is " + fav_Color)
+
+
+//case sensitive
+// 1: camelCase
+// 2: PascalCase
+// 3: snake_case
+// 4: kebab-case
+// only before $ and _ can be used
+
+
+// strong typing
+let userName : string = "Aftab"
+console.log(userName)
+
+// here i can only use string
+// userName = 1    // its giving error
