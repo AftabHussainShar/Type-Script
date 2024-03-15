3951c5c3bf7852a3996248ff0d8a01c122760183
Aftab Hussain <96955752+AftabHussainShar@users.noreply.github.com>
2024-03-15 14:27:07 +0500

    Data-Type

---
+++ 02-Data-Types/main.js
@@ -0,0 +1,18 @@
+"use strict";
+// Data Types 
+// 1:String
+// 2:Number
+// 3:Boolean
+// 4:Any 
+// 1:String 
+let Username = "sachin";
+console.log(Username);
+// 2:Number
+let Age = 25;
+console.log(Age);
+// 3:Boolean
+let IsMarried = true;
+console.log(IsMarried);
+// 4:Any
+let Car = "BMW";
+console.log(Car);
---
+++ 02-Data-Types/main.ts
@@ -0,0 +1,22 @@
+// Data Types 
+// 1:String
+// 2:Number
+// 3:Boolean
+// 4:Any 
+
+
+// 1:String 
+let Username:string = "sachin";
+console.log(Username);
+
+// 2:Number
+let Age:number = 25;
+console.log(Age);
+
+// 3:Boolean
+let IsMarried:boolean = true;
+console.log(IsMarried);
+
+// 4:Any
+let Car:any = "BMW";
+console.log(Car);
