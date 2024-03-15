b6c3bc6a1c27d8c14ef2d722becb30c58e7ed3bc
Aftab Hussain <96955752+AftabHussainShar@users.noreply.github.com>
2024-03-15 14:31:45 +0500

    Type-Infrence

---
+++ 03-Type-Inference/main.js
@@ -0,0 +1,16 @@
+"use strict";
+// Type Inference 
+// 1:Strongly Typed
+let myName = "John";
+myName = "Jane";
+let myAge = 30;
+myAge = 31;
+let isActive = true;
+isActive = false;
+// 2:Type Inference
+let myName2 = "John";
+myName2 = "Jane";
+let myAge2 = 30;
+myAge2 = 31;
+let isActive2 = true;
+isActive2 = false;
---
+++ 03-Type-Inference/main.ts
@@ -0,0 +1,20 @@
+// Type Inference 
+
+
+// 1:Strongly Typed
+let myName: string = "John";
+myName = "Jane";
+let myAge: number = 30;
+myAge = 31;
+let isActive: boolean = true;
+isActive = false;
+
+
+// 2:Type Inference
+let myName2 = "John";
+myName2 = "Jane";
+let myAge2 = 30;
+myAge2 = 31;
+let isActive2 = true;
+isActive2 = false;
+
