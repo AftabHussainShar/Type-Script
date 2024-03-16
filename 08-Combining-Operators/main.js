f9f1419d5639d0ed041d518b9d96e860d2d8c52f
Aftab Hussain <96955752+AftabHussainShar@users.noreply.github.com>
2024-03-17 04:37:07 +0500

    Combining Operators

---
+++ 08-Combining-Operators/main.js
@@ -0,0 +1,15 @@
+var result = 10 + 10 - 10 * 10 / 10;
+console.log(result); // 10
+// now use combing operators with prefix and postfix
+var result1 = 10 + 10;
+result1 = ++result1;
+console.log(result1); // 20
+var result2 = 10 - 10;
+result2 = --result2;
+console.log(result2); // 0
+var result3 = 10 * 10;
+result3 = result3++;
+console.log(result3); // 100
+var result4 = 10 / 10;
+result4 = result4--;
+console.log(result4); // 1
---
+++ 08-Combining-Operators/main.ts
@@ -0,0 +1,18 @@
+let result = 10 + 10 - 10 * 10 / 10
+console.log(result)  // 10
+
+
+// now use combing operators with prefix and postfix
+
+let result1 = 10 + 10
+result1 = ++result1
+console.log(result1)  // 21
+let result2 = 10 - 10
+result2 = --result2
+console.log(result2)  //-1
+let result3 = 10 * 10
+result3 = result3++
+console.log(result3)  // 100
+let result4 = 10 / 10
+result4 = result4--
+console.log(result4)  // 1
