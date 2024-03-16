fa122b5eecebb8bc497dbee26605b8ecf302bb08
Aftab Hussain <96955752+AftabHussainShar@users.noreply.github.com>
2024-03-17 04:29:36 +0500

    Operators

---
+++ 07-Operators/main.ts
@@ -0,0 +1,31 @@
+// 1: Addition 
+
+let x = 10 + 10;  // it will give 20
+console.log(x);
+
+// 2: Subtraction
+
+let y = 10 - 10;  // it will give 0
+console.log(y);
+
+// 3: Multiplication
+
+let z = 10 * 10;  // it will give 100
+console.log(z);
+
+// 4: Division
+
+let a = 10 / 10;  // it will give 1
+console.log(a);
+
+
+// 5: Exponnents
+
+let b = 10 ** 10;  // it will give 100000000000
+console.log(b)
+
+
+// 6: Modulus
+
+let c = 10 % 10;  // it will give 0
+console.log(c)
