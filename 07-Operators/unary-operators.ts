8f854649b170af4886cc2ac145e826593598c415
Aftab Hussain <96955752+AftabHussainShar@users.noreply.github.com>
2024-03-17 04:32:57 +0500

    Unary Operators

---
+++ 07-Operators/unary-operators.ts
@@ -0,0 +1,25 @@
+// 1 : Prefix Increment
+
+let a1 = 1
+let b1 = ++a1
+console.log(b1)  // 2
+
+// 2 : Prefix Decrement
+
+let a2 = 1
+let b2 = --a2
+console.log(b2)  // 0
+
+
+// 3 : Postfix Increment
+
+let a3 = 1
+let b3 = a3++
+console.log(b3)  // 1
+
+
+// 4 : Postfix Decrement
+
+let a4 = 1
+let b4 = a4--
+console.log(b4)  // 1
