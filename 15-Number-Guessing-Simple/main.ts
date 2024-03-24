158e9e47f18bf19dada4409e710ce05543d6143a
Aftab Hussain <96955752+AftabHussainShar@users.noreply.github.com>
2024-03-24 22:54:30 +0500

    update

--- 15-Number-Guessing-Simple/main.ts
+++ 15-Number-Guessing-Simple/main.ts
@@ -14,6 +14,7 @@ const answer = await inquirer.prompt([
 // generate random number
 let number2: number = Math.floor(Math.random() * 10) + 1
 
+// if condition
 if (answer.number ===number2 ) {
     console.log("You guessed right")
     console.log("The number was " + number2)
