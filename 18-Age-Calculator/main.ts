4fc7fa2c29a6e12f1164f0f962f0ec90e473a92a
Aftab Hussain <96955752+AftabHussainShar@users.noreply.github.com>
2024-03-30 15:00:42 +0500

    Update main.ts

--- 18-Age-Calculator/main.ts
+++ 18-Age-Calculator/main.ts
@@ -1,2 +1,7 @@
 // # take one date of date of birth then calculate age of person
 import inquirer from "inquirer"
+
+// take date of birth
+const answer = await inquirer.prompt([
+    {message: "Enter your date of birth", type: "date", name: "dob"}
+])
